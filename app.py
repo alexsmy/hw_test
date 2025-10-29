a1b310a609d13159b4ee3da31e31a25747158fd5
alexsmy <135685932+alexsmy@users.noreply.github.com>
2025-10-30 00:04:53 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Привет! Ваше приложение на Python успешно работает на Railway!</h1>'
+
+if __name__ == "__main__":
+
+    port = int(os.environ.get('PORT', 8080))
+
+    app.run(host='0.0.0.0', port=port)
